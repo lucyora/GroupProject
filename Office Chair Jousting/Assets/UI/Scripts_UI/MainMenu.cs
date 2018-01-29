52f42ed4662bdfd4d901adeae7420f0e04915ec6
lucyora <32249927+lucyora@users.noreply.github.com>
2018-01-29 18:58:29 -0500

    Menu UI updated

--- Office Chair Jousting/Assets/UI/Scripts_UI/MainMenu.cs
+++ Office Chair Jousting/Assets/UI/Scripts_UI/MainMenu.cs
@@ -10,6 +10,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void MatchGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
     public void QuitGame()
     {
         Debug.Log("QUIT!");
