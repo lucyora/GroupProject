322dc28d2ea1f798a449cef10ce20b1a01aa41a3
ZackWeinstein <32457340+ZackWeinstein@users.noreply.github.com>
2018-02-02 12:03:00 -0500

    Inital Controller Script Commit
    Currently working on player rotation. Double to Float conversion on arctan is causing issues

---
+++ Office Chair Jousting/Assets/Scripts/Controller.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+ 
+using UnityEngine;
+ 
+public class Controller : MonoBehaviour {
+    public enum current_player {Player_1,Player_2,Player_3,Player_4,AI};
+    public current_player Current_Player;
+    private bool isAI;
+    private string P1_LX = "Horizontal";
+    private string P1_LY = "Vertical";
+ 
+    private string P1_RX = "Horizontal2";
+    private string P1_RY = "Vertical2";
+    //Placeholder Values. Fill in with appropriate axis labels later
+    private string P2_LX = "2Horizontal";
+    private string P2_LY = "2Vertical";
+ 
+    private string P2_RX = "2Horizontal2";
+    private string P2_RY = "2Vertical2";
+ 
+    private string P3_LX = "3Horizontal";
+    private string P3_LY = "3Vertical";
+ 
+    private string P3_RX = "3Horizontal2";
+    private string P3_RY = "3Vertical2";
+ 
+    private string P4_LX = "4Horizontal";
+    private string P4_LY = "4Vertical";
+ 
+    private string P4_RX = "4Horizontal2";
+    private string P4_RY = "4Vertical2";
+    //
+ 
+    //Strings to be used once player is determined
+    private string SelectedP_LX;
+    private string SelectedP_LY;
+ 
+    private string SelectedP_RX;
+    private string SelectedP_RY;
+ 
+ 
+ 
+ 
+ 
+ 
+    // Use this for initialization
+    void Start () {
+        switch (Current_Player)
+        {
+            case current_player.Player_1:
+                SelectedP_LX = P1_LX;
+                SelectedP_LY = P1_LY;
+                SelectedP_RX = P1_RX;
+                SelectedP_RY = P1_RY;
+                break;
+            case current_player.Player_2:
+                SelectedP_LX = P2_LX;
+                SelectedP_LY = P2_LY;
+                SelectedP_RX = P2_RX;
+                SelectedP_RY = P2_RY;
+                break;
+            case current_player.Player_3:
+                SelectedP_LX = P3_LX;
+                SelectedP_LY = P3_LY;
+                SelectedP_RX = P3_RX;
+                SelectedP_RY = P3_RY;
+                break;
+            case current_player.Player_4:
+                SelectedP_LX = P4_LX;
+                SelectedP_LY = P4_LY;
+                SelectedP_RX = P4_RX;
+                SelectedP_RY = P4_RY;
+                break;
+            case current_player.AI:
+                SelectedP_LX = "AI";
+                SelectedP_LY = "AI";
+                SelectedP_RX = "AI";
+                SelectedP_RY = "AI";
+                isAI = true;
+                break;
+        }
+       
+    }
+   
+    // Update is called once per frame
+    void Update () {
+        double rotation = Math.Atan2(Math.Round(Input.GetAxis(SelectedP_LY), 2), Math.Round(Input.GetAxis(SelectedP_LX), 2)) * 180 / Math.PI;
+        Debug.Log(rotation);
+        transform.rotation = new Quaternion(transform.rotation.x, (float)rotation, transform.rotation.z,transform.rotation.w);
+    }
+}
