3b8d3970baf24d82efc6c515ef7252dd47ba0663
NikoMellic <Niko_video98@Hotmail.com>
2018-02-09 11:30:51 -0500

    Modification to office level + elevator script

---
+++ Office Chair Jousting/Assets/Elevator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{ 
+    public GameObject Position;
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            other.transform.position = Position.transform.position;
+        }
+    }
+}
