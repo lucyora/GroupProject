c7c90bc8cc2d91447afa5d1592a157d376560996
SLCTECH\syoon <lucyora@gmail.com>
2018-02-12 12:13:33 -0500

    Added UI Language for test ( parsing from json files - current font issues of using Korean, Hindi )

---
+++ Office Chair Jousting - lanTest/Office Chair Jousting/Assets/Scripts/Raycast.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour {
+    public float downlength;
+    public float leftlength;
+    public float fwdlength;
+    public float backlength;
+    public float rightlength;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        RaycastHit dhit;
+        RaycastHit lhit;
+        RaycastHit rhit;
+        RaycastHit fhit;
+        RaycastHit bhit;
+
+        Ray downRay = new Ray(transform.position, -transform.up);
+        Ray leftRay = new Ray(transform.position, -transform.right);
+        Ray rgtRay = new Ray(transform.position, transform.right);
+        Ray fwdRay = new Ray(transform.position, transform.forward);
+        Ray backRay = new Ray(transform.position, -transform.forward);
+
+        Debug.DrawRay(transform.position, -transform.up * downlength);
+        Debug.DrawRay(transform.position, -transform.right * leftlength);
+        Debug.DrawRay(transform.position, transform.right * rightlength);
+        Debug.DrawRay(transform.position, transform.forward * fwdlength);
+        Debug.DrawRay(transform.position, -transform.forward * backlength);
+
+        Physics.Raycast(leftRay, out lhit, leftlength);
+        Physics.Raycast(rgtRay, out rhit, rightlength);
+        Physics.Raycast(fwdRay, out fhit, fwdlength);
+        Physics.Raycast(backRay, out bhit, backlength);
+
+        if (Physics.Raycast(downRay, out dhit, downlength))
+        {
+            if (dhit.collider.tag == "Ground" )
+                Debug.Log("On ground");
+        }
+        else if (dhit.collider == null && lhit.collider == true || rhit.collider == true || fhit.collider == true || bhit.collider == true)
+        {
+            Debug.Log("Prone");
+        }
+        
+	}
+}
---
+++ Office Chair Jousting/Assets/UI/Scripts_UI/LocalizedText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LocalizedText : MonoBehaviour
+{
+    public string key;
+    private TextMeshProUGUI m_Text;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_Text = GetComponent<TextMeshProUGUI>();
+        string temp = LocalizationManager.instance.GetLocalizedValue(key);
+        m_Text.SetText(temp);   //Replace text form the localization file
+    }
+
+}
