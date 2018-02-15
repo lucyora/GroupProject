ad0bde93db341522867e11d2e1b0494b05374e60
Brijesh Shir <shir.brijesh1992@live.com>
2018-02-14 19:28:26 -0500

    Chair model created using hinge and fixed joint

---
+++ Office Chair Jousting/Assets/Scripts/MoveChair.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveChair : MonoBehaviour {
+    private Rigidbody rb;
+    //public Transform Wheel1;
+    //public Transform Wheel2;
+    //public Transform Wheel3;
+    //public Transform Wheel4;
+    //public Transform Wheel5;
+    public float torque;
+    public float speed;
+    private float velocitycap = 100.0f;
+
+    public float steerAngle { get; private set; }
+
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        speed = 10.0f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+        steerAngle = Input.GetAxis("Horizontal") ;
+
+        //rb.AddForce(transform.forward * speed);
+        transform.Translate(speed * Input.GetAxis("Horizontal") * Time.deltaTime, 0.0f, speed * Input.GetAxis("Vertical") * Time.deltaTime);
+        //GetComponent<Rigidbody>().velocity = new Vector3(Mathf.Clamp((GetComponent<Rigidbody>().velocity.x - Input.GetAxis("Vertical")), -velocitycap, velocitycap), GetComponent<Rigidbody>().velocity.y, Mathf.Clamp((GetComponent<Rigidbody>().velocity.z - Input.GetAxis("Horizontal")), -velocitycap, velocitycap));
+
+        //Wheel1.localEulerAngles = new Vector3(Wheel1.localEulerAngles.x, steerAngle - Wheel1.localEulerAngles.z, Wheel1.localEulerAngles.z);
+    }
+}
---
+++ Office Chair Jousting/Assets/Scripts/VRToggle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class VRToggle : MonoBehaviour {
+
+
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            ToggleVR();
+        }        
+    }
+
+    void ToggleVR()
+    {
+        if(UnityEngine.VR.VRSettings.loadedDeviceName == "Oculus")
+        {
+            StartCoroutine(LoadDevice("None"));
+            Debug.Log("None");
+        }
+        else
+        {
+            StartCoroutine(LoadDevice("Oculus"));
+            Debug.Log("Oculus");
+        }
+    }
+
+    IEnumerator LoadDevice(string newDevice)
+    {
+        UnityEngine.VR.VRSettings.LoadDeviceByName(newDevice);
+        yield return null;
+        UnityEngine.VR.VRSettings.enabled = true;
+    }
+}
