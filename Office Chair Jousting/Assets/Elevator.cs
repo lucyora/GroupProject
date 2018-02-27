1d5fbd07879a9cbe2e182dd351269f490a3847cb
NikoMellic <Niko_video98@Hotmail.com>
2018-02-27 17:36:45 -0500

    Office level npcs

--- Office Chair Jousting/Assets/Elevator.cs
+++ Office Chair Jousting/Assets/Elevator.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class Elevator : MonoBehaviour
 {
-    public GameObject Destination;
+    public Transform Destination;
      void OnTriggerEnter(Collider other)    // once the trigger on the collider activates 
     {
         if (other.gameObject.tag == "Player") // and the other object's tag is "Player"
         {
             other.gameObject.transform.position = Destination.transform.position; // set the player's position to the destination item's.
+            other.gameObject.transform.rotation = Destination.transform.rotation; // set the player's position to the destination item's.
         }
     }
 }
