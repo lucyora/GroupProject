a7b6d1a90bd6a9e43c0efacaaaf39e37492be1b3
SLCTECH\jmcalear <jmca101@hotmail.com>
2018-03-09 10:51:33 -0500

    Fix

--- Office Chair Jousting/Assets/Scripts/AI.cs
+++ Office Chair Jousting/Assets/Scripts/AI.cs
@@ -1,25 +1,43 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.AI;
+using UnityEngine;
+
 
 public class AI : MonoBehaviour
 {
+
 
     public static Controller controllerInstance;
     public static Raycast raycastInstance;
     public static Controller controller;
     public GameObject player;
-    private NavMeshAgent _nav;
-    private Transform _player;
+    private Transform playerTransform;
+    private NavMeshAgent nav;
 
-    void Start()
-    {
-        _nav = GetComponent<NavMeshAgent>();
-        _player = GameObject.FindGameObjectWithTag("Player").transform;
-    }
 
-    void Update()
+	// Use this for initialization
+	void Awake ()
     {
-        Debug.Log("In the Update");
-        _nav.SetDestination(_player.position);
-    }
+        player = GameObject.FindGameObjectWithTag("Player");
+        nav = GetComponent<NavMeshAgent>();
+        Debug.Log("I AM AWAKE");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       
+        if(raycastInstance.isOBJAlive() == true)
+        { 
+            Vector3 direction = playerTransform.position - this.transform.position;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+            nav.enabled = true;
+            Debug.Log("I AM MOVING");
+        }
+        else
+        {
+            nav.enabled = false;
+        }
+	}
 }
