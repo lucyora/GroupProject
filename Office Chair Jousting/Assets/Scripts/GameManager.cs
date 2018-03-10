eaa01e317b6839bfa69b7de437483c666a4e16c5
ZackWeinstein <w.zack@outlook.com>
2018-03-10 17:22:49 -0500

    Preliminary GameManager +Player & AI prefab

---
+++ Office Chair Jousting/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    private GameObject SpawnArea;
+    public GameObject Player;
+    public GameObject AI;
+    public bool Spawn;
+	// Use this for initialization
+	void Start () {
+        SpawnArea = GameObject.FindGameObjectWithTag("SpawnArea");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Spawn)
+        {
+            SpawnPlayer(true);
+            Spawn = false;
+        }
+		
+	}
+    void SpawnPlayer(bool isAI)
+    {
+        Vector3 Center = SpawnArea.GetComponent<BoxCollider>().center;
+        float max = SpawnArea.GetComponent<BoxCollider>().bounds.max.x;
+        Vector3 spawnposition = new Vector3(Random.Range(-max,max),0.0f,Random.Range(-max, max));
+        Instantiate(AI, spawnposition,Quaternion.identity);
+    }
+}
