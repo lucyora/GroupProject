fabd28e817690222e739f77ea830486d88ee62f4
DESKTOP-82A48MT\jmca1 <jmca101@hotmail.com>
2018-03-16 08:28:15 -0400

    MainMenu Polish

---
+++ Office Chair Jousting/Assets/UI/Scripts_UI/LoadStartGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadStartGame : MonoBehaviour {
+    
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadSceneAsync(sceneName);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    } 
+
+}
