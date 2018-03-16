40daa9f94e6da25e0e578e11a1d4740d39c9da6a
BrennAdam <brenn.adam@gmail.com>
2018-03-16 11:47:16 -0400

    AI chases player

--- Office Chair Jousting/Assets/Scripts/AI.cs
+++ Office Chair Jousting/Assets/Scripts/AI.cs
@@ -8,24 +8,34 @@ public class AI : Player
 {
 
     public GameObject player;
+    public float speed;
     private NavMeshAgent nav;
 
 	// Use this for initialization
 	void Awake ()
     {
         Current_Player = current_player.AI;//Stops any controller from being set to this player
-        //player = GameObject.FindGameObjectWithTag("Player");
+        player = GameObject.FindGameObjectWithTag("Player");
         nav = GetComponent<NavMeshAgent>();
         Debug.Log("I AM AWAKE");
         nav.enabled = true;
         isAlive = true;
     }
-	
-	// Update is called once per frame
-	public override void UpdatePosition()//Overrides the player UpdatePosition function that handles player input. Raycasting for death is handled in player.cs
+
+
+    //add deviation stufff
+    // Update is called once per frame
+    public override void UpdatePosition()//Overrides the player UpdatePosition function that handles player input. Raycasting for death is handled in player.cs
     {
-            Vector3 direction = player.transform.position - this.transform.position;
-            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
-            //Debug.Log("I AM MOVING");
-	}
+        Vector3 target = player.transform.position;// this.transform.position;
+        //this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+//        Quaternion offset = Quaternion.Euler(0, -45, 0);
+        transform.LookAt(target);
+       //transform.rotation = Quaternion.LookRotation(direction) * offset;
+        transform.position += transform.forward * speed * Time.deltaTime;
+        transform.rotation *= Quaternion.Euler(0, -90, 0);
+        //Debug.Log(direction);
+
+    }
+
 }
