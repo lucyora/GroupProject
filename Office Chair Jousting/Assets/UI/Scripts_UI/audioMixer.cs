01539eb8b5d84ab5be3d6917f2b16ceec6eef3df
DESKTOP-82A48MT\jmca1 <jmca101@hotmail.com>
2018-03-25 15:40:50 -0400

    Audio Mixer + finished Polish

---
+++ Office Chair Jousting/Assets/UI/Scripts_UI/audioMixer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class audioMixer : MonoBehaviour {
+
+    public AudioMixer masterMixer;
+
+    public void setFxVolume(float FxVolume)
+    {
+        FxVolume = FxVolume * 20;
+        masterMixer.SetFloat("EffectsParam", FxVolume);
+    }
+    public void setMusicVolume(float musicVolume)
+    {
+        musicVolume = musicVolume * 20;
+        masterMixer.SetFloat("MusicParam", musicVolume);
+    }
+    public void setMasterVolume(float MasterVolume)
+    {
+        MasterVolume = MasterVolume *20;
+        masterMixer.SetFloat("MasterParam", MasterVolume);
+    }
+}
