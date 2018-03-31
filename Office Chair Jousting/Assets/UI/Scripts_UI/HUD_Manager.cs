4ab8d16d8dd4fc9e1a68cc86cff20ccb91368783
lucyora <32249927+lucyora@users.noreply.github.com>
2018-03-31 02:39:30 -0400

    Scoring update on HUD using with the new test scene.

--- Office Chair Jousting/Assets/UI/Scripts_UI/HUD_Manager.cs
+++ Office Chair Jousting/Assets/UI/Scripts_UI/HUD_Manager.cs
@@ -16,10 +16,9 @@ public class HUD_Manager : MonoBehaviour
 
     public float TimeLeft;
     public bool SurvivalMode;
+
     public TextMeshProUGUI TimerText;
-
     public TextMeshProUGUI GameStatusText;
-
     public TextMeshProUGUI P1score;
     public TextMeshProUGUI P2score;
     public TextMeshProUGUI P3score;
@@ -41,7 +40,7 @@ public class HUD_Manager : MonoBehaviour
 
 
         //Test get player hud object
-        Testplayer = GameObject.Find("HUDCanvas/HUDManager/Player1");
+        //Testplayer = GameObject.Find("HUDCanvas/HUDManager/Player1");
 
     }
 
@@ -52,12 +51,11 @@ public class HUD_Manager : MonoBehaviour
         InitHudTxt();
 
         //TODO: set player img
-        // Player1 == Player.character.Jenny...
+        // Player1 == Player.character.Jenny... etc
     }
     // Update is called once per frame
     void Update ()
     {
-
         //Timer
         TimeLeft -= Time.deltaTime;
 
@@ -81,45 +79,47 @@ public class HUD_Manager : MonoBehaviour
 
         string temp = "Time " + min.ToString("00") + ":" + sec.ToString("00");
         TimerText.text = temp;
-  
-        //TODO: think about localization...
-        //m_Text = GetComponent<TextMeshProUGUI>();
-        //string temp = LocalizationManager.instance.GetLocalizedValue(key);
-        //m_Text.SetText(temp);   //Replace text form the localization file
 
+        //Score
+        ScoreUpdateHUD();
 
         if (TimeLeft < 0 || GameStatus == (int)MatchMode.MatchDone) //Time out or match done
         {
+            TimeLeft = 0;
             GameOver();
         }
 
         //Test for checking player death
-        if (!playerClass.isAlive)
-        {
-            GameOver();
-            Debug.Log("Player1's gone far away...");
-        }
+        //if (!playerClass.isAlive)
+        //{
+        //    GameOver();
+        //    Debug.Log("Player1's gone far away...");
+        //}
 
         //TODO: Think about pop up hud with sub-menu to choose restart
-        if (Input.GetButtonDown("JoyA"))
-        {
-            //GameOver();
-        }
-        else if (Input.GetButtonDown("JoyB"))
-        {
-            //GameStart();
-            GameStatusText.text = "Test";
-        }
+        //if (Input.GetButtonDown("JoyA"))
+        //{
+        //    //GameOver();
+        //}
+        //else if (Input.GetButtonDown("JoyB"))
+        //{
+        //    //GameStart();
+        //    GameStatusText.text = "Test";
+        //}
 
     }
     void ScoreUpdateHUD()
     {
-        //TODO: Keep tracking players' score.
-        P1score.text = "Score : " + Scores;
+        //Temporary scores setting
+        float ps1 = gamemanager.score[0];
+        float ps2 = gamemanager.score[1];
+        float ps3 = gamemanager.score[2];
+        float ps4 = gamemanager.score[3];
 
-       
-        
-
+        P1score.text = "Score : " + ps1; 
+        P2score.text = "Score : " + ps2;
+        P3score.text = "Score : " + ps3;
+        P4score.text = "Score : " + ps4;
     }
 
     void GameOver()
