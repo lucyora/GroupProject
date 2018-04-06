63ae27facd7d1597a3662c943b3735fe65eb5276
Brijesh Shir <shir.brijesh1992@live.com>
2018-04-06 11:56:11 -0400

    Multi player camera change focus according to distance between instantiated players at runtime

--- Office Chair Jousting/Assets/Scripts/MultiPlayerCamera.cs
+++ Office Chair Jousting/Assets/Scripts/MultiPlayerCamera.cs
@@ -10,7 +10,7 @@ using UnityEngine;
 [RequireComponent(typeof(Camera))]
 public class MultiPlayerCamera : MonoBehaviour
 {
-    public List<Transform> targets = new List<Transform>();                         // Dynamic target list to follow.It allows add as many player as controller connected.
+    public List<GameObject> targets = new List<GameObject>();  // Dynamic target list to follow.It allows add as many player as controller connected.
     public Vector3 offset;                                  // Offset the camera to keep every player inside the range
     public float minZoom = 10.0f;
     public float maxZoom = 60.0f;
@@ -21,29 +21,23 @@ public class MultiPlayerCamera : MonoBehaviour
     private Camera cam;
 
     private Player player;
+    private GameManager gameManager;
 
     private void Start()
     {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
         cam = GetComponent<Camera>();
     }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            targets.Add(player.gameObject.transform);
-        }
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            targets.Remove(player.gameObject.transform);
-        }
-
+        targets = gameManager.PlayerList;  
     }
 
 
     private void LateUpdate()
     {
         if (targets.Count == 0)                             // Null check condition, if there is no player to follow for camera it just returns. 
-        {
+        {  
             return;
         }
 
@@ -55,13 +49,13 @@ public class MultiPlayerCamera : MonoBehaviour
     {
         if (targets.Count == 1)                             //If there is only one target no need to keep track of center, but still needs to follow the player
         {
-            return targets[0].position;
+            return targets[0].transform.position;
         }
 
-        var bounds = new Bounds(targets[0].position, Vector3.zero);         // Creates the boundary around all connected player.
+        var bounds = new Bounds(targets[0].transform.position, Vector3.zero);         // Creates the boundary around all connected player.
         for (int i = 0; i < targets.Count; i++)
         {
-            bounds.Encapsulate(targets[i].position);        // Resizes the box according to the targets
+            bounds.Encapsulate(targets[i].transform.position);        // Resizes the box according to the targets
         }
         return bounds.center;                               // Returns the center point of the boundary
     }
@@ -78,10 +72,10 @@ public class MultiPlayerCamera : MonoBehaviour
 
     float GetGreatestDiatance()                             // Finds out the distance between players to adjust zoom and movements of the camera.
     {
-        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        var bounds = new Bounds(targets[0].transform.position, Vector3.zero);
         for (int i = 0; i < targets.Count; i++)
         {
-            bounds.Encapsulate(targets[i].position);
+            bounds.Encapsulate(targets[i].transform.position);
         }
         return bounds.size.z;
     }
