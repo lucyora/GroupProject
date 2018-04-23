277d52bfbdceadbbb582fc2625c86e8e98b5b89a
lchan <32458724+324108430@users.noreply.github.com>
2018-04-23 19:02:34 -0400

    Space level is done with "zero" gravity

---
+++ Office Chair Jousting/Assets/Gravity.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour {
+    void Example()
+    {
+        Physics.gravity = new Vector3(0, -3.0F, 0);
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Example();
+	}
+}
