cc50f5a7dc09a6551c0e82c1d2baa403eef1a435
DESKTOP-82A48MT\jmca1 <jmca101@hotmail.com>
2018-04-27 09:44:43 -0400

    Credit Back Button

---
+++ Office Chair Jousting/Assets/UI/Scripts_UI/CreditsBack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class CreditsBack : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < 4; i++)
+        {
+            if (Input.GetButtonDown("JoyB"+i))
+            {
+                SceneManager.LoadSceneAsync("MainMenu");
+            }
+        }
+	}
+}
